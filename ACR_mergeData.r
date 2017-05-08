08439e0bd2af199af647a02cc08633153b41b16c
Claire Runge <runge@nceas.ucsb.edu>
2017-05-08 11:44:37 -0700

    conversion now annual avereaged across years

--- ACR_mergeData.r
+++ ACR_mergeData.r
@@ -52,7 +52,7 @@ library(plyr)
 
 #area of rangeland from LARK et al. 2015
 	rangelandAreaDF <- read.csv("Cropland and pasture/Cropland and Rangeland by year/Rangeland_Area_byYear_ha_2008to2015.csv")
-	rangelandAreaAllcounties <- rangelandAreaDF[ , c("ADMIN_FIPS", "Rangeland_2008_ha", "Rangeland_2009_ha", "Rangeland_2010_ha", "Rangeland_2011_ha", "Rangeland_2012_ha", "Rangeland_2013_ha", "Rangeland_2014_ha", "Rangeland_2015_ha", "X2009_RangetoRange_ha", "X2010_RangetoRange_ha", "X2011_RangetoRange_ha", "X2012_RangetoRange_ha", "X2013_RangetoRange_ha", "X2014_RangetoRange_ha", "X2015_RangetoRange_ha", "TotalArea_RangeorCrop_ha")] #select only relevant columns
+	rangelandAreaAllcounties <- rangelandAreaDF[ , c("ADMIN_FIPS", "Stable.area.of.noncrop", "Rangeland_2008_ha", "Rangeland_2009_ha", "Rangeland_2010_ha", "Rangeland_2011_ha", "Rangeland_2012_ha", "Rangeland_2013_ha", "Rangeland_2014_ha", "Rangeland_2015_ha", "X2009_RangetoRange_ha", "X2010_RangetoRange_ha", "X2011_RangetoRange_ha", "X2012_RangetoRange_ha", "X2013_RangetoRange_ha", "X2014_RangetoRange_ha", "X2015_RangetoRange_ha", "TotalArea_RangeorCrop_ha")] #select only relevant columns
 	rangelandAreaAllcounties <- rangelandAreaAllcounties[order(rangelandAreaAllcounties$ADMIN_FIPS),] #order data by fips
 CropRangeDF <- merge(croplandAreaAllcounties, rangelandAreaAllcounties, by="ADMIN_FIPS", all.x=TRUE)
 
@@ -72,7 +72,7 @@ LCC1to6RangeDFnew <- read.csv("Cropland and pasture/Cropland and Rangeland by LC
 LCC1to6CropDFnew <- read.csv("Cropland and pasture/Cropland and Rangeland by LCC/CroplandArea_byYear_2015_LCC1to6.csv", header=TRUE)
 LCC1to6Merge <- merge(LCC1to6RangeDF, LCC1to6CropDF[, !names(LCC1to6CropDF) %in% c("NAME")], by=c("ADMIN_FIPS", "STATE"), all.x=TRUE)
 LCC1to6Mergenew <- merge(LCC1to6RangeDFnew, LCC1to6CropDFnew[, !names(LCC1to6CropDFnew) %in% c("NAME")], by=c("ADMIN_FIPS", "STATE"), all.x=TRUE)
-LCC1to6Merge <- merge(LCC1to6Merge, LCC1to6Mergenew[, !names(LCC1to6Mergenew) %in% c("Total_Area_RangeorCrop_inLCC1to6", "Stable_cropland")], by=c("ADMIN_FIPS", "STATE"), all.x=TRUE)
+LCC1to6Merge <- merge(LCC1to6Merge, LCC1to6Mergenew[, !names(LCC1to6Mergenew) %in% c("Total_Area_RangeorCrop_inLCC1to6", "Stable_cropland", "Stable.area.of.noncrop")], by=c("ADMIN_FIPS", "STATE"), all.x=TRUE)
 
 LCC5or6RangeDF <- read.csv("Cropland and pasture/Cropland and Rangeland by LCC/Rangeland_Area_byYear_LCC5or6.csv", header=TRUE)
 LCC5or6CropDF <- read.csv("Cropland and pasture/Cropland and Rangeland by LCC/CroplandArea_byYear_LCC5or6.csv", header=TRUE)
@@ -80,14 +80,14 @@ LCC5or6RangeDFnew <- read.csv("Cropland and pasture/Cropland and Rangeland by LC
 LCC5or6CropDFnew <- read.csv("Cropland and pasture/Cropland and Rangeland by LCC/CroplandArea_byYear_2015_LCC5or6.csv", header=TRUE)
 LCC5or6Merge <- merge(LCC5or6RangeDF, LCC5or6CropDF[, !names(LCC5or6CropDF) %in% c("NAME")], by=c("ADMIN_FIPS", "STATE"), all.x=TRUE)
 LCC5or6Mergenew <- merge(LCC5or6RangeDFnew, LCC5or6CropDFnew[, !names(LCC5or6CropDFnew) %in% c("NAME")], by=c("ADMIN_FIPS", "STATE"), all.x=TRUE)
-LCC5or6Merge <- merge(LCC5or6Merge, LCC5or6Mergenew[, !names(LCC5or6Mergenew) %in% c("Total_Area_RangeorCrop_inLCC5or6", "Stable_cropland")], by=c("ADMIN_FIPS", "STATE"), all.x=TRUE)
+LCC5or6Merge <- merge(LCC5or6Merge, LCC5or6Mergenew[, !names(LCC5or6Mergenew) %in% c("Total_Area_RangeorCrop_inLCC5or6", "Stable_cropland", "Stable.area.of.noncrop")], by=c("ADMIN_FIPS", "STATE"), all.x=TRUE)
 
 CropRangeLCCLong <- rbind(with(CropRangeDF,	
 								data.frame(
 									ADMIN_FIPS=rep(ADMIN_FIPS, 7), 
 									State=rep(STATE, 7), 
 									Year=rep(c(2009, 2010, 2011, 2012, 2013, 2014, 2015), each=nrow(CropRangeDF)),	
-									TotalRangeorCropAreainLCC_ha = rep(TotalArea_RangeorCrop_ha, 7),
+									StableArea_noncrop= rep(Stable.area.of.noncrop, 7),TotalRangeorCropAreainLCC_ha = rep(TotalArea_RangeorCrop_ha, 7),
 									LCC = rep("AllLCC", 7*nrow(CropRangeDF)),
 									Area_cropland = c(X2008_Cropland_area_ha, X2009_Cropland_area_ha, X2010_Cropland_area_ha, X2011_Cropland_area_ha, X2012_Cropland_area_ha, X2013_Cropland_area_ha, X2014_Cropland_area_ha),
 									Area_Ranget0_to_Cropt1= c(X2009_RangetoCropland_area_ha, X2010_RangetoCropland_area_ha, X2011_RangetoCropland_area_ha, X2012_RangetoCropland_area_ha, X2013_RangetoCropland_area_ha, X2014_RangetoCropland_area_ha, X2015_RangetoCropland_area_ha),
@@ -99,7 +99,7 @@ CropRangeLCCLong <- rbind(with(CropRangeDF,
 									ADMIN_FIPS=rep(ADMIN_FIPS, 7), 
 									State=rep(STATE, 7), 
 									Year=rep(c(2009, 2010, 2011, 2012, 2013, 2014, 2015), each=nrow(CropRangeDF)),	
-									TotalRangeorCropAreainLCC_ha = rep(Total_Area_RangeorCrop_inLCC1to6, 7),
+									StableArea_noncrop= rep(Stable.area.of.noncrop, 7),TotalRangeorCropAreainLCC_ha = rep(Total_Area_RangeorCrop_inLCC1to6, 7),
 									LCC = rep("LCC1to6", 7*nrow(LCC1to6Merge)),
 									Area_cropland = c(X2008_Cropland_area_ha, X2009_Cropland_area_ha, X2010_Cropland_area_ha, X2011_Cropland_area_ha, X2012_Cropland_area_ha, X2013_Cropland_area_ha, X2014_Cropland_area_ha),
 									Area_Ranget0_to_Cropt1= c(X2009_RangetoCropland_area_ha, X2010_RangetoCropland_area_ha, X2011_RangetoCropland_area_ha, X2012_RangetoCropland_area_ha, X2013_RangetoCropland_area_ha, X2014_RangetoCropland_area_ha, X2015_RangetoCropland_area_ha),
@@ -111,7 +111,7 @@ CropRangeLCCLong <- rbind(with(CropRangeDF,
 									ADMIN_FIPS=rep(ADMIN_FIPS, 7), 
 									State=rep(STATE, 7), 
 									Year=rep(c(2009, 2010, 2011, 2012, 2013, 2014, 2015), each=nrow(CropRangeDF)),	
-									TotalRangeorCropAreainLCC_ha = rep(Total_Area_RangeorCrop_inLCC5or6, 7),
+									StableArea_noncrop= rep(Stable.area.of.noncrop, 7),TotalRangeorCropAreainLCC_ha = rep(Total_Area_RangeorCrop_inLCC5or6, 7),
 									LCC = rep("LCC5or6", 7*nrow(LCC5or6Merge)),
 									Area_cropland = c(X2008_Cropland_area_ha, X2009_Cropland_area_ha, X2010_Cropland_area_ha, X2011_Cropland_area_ha, X2012_Cropland_area_ha, X2013_Cropland_area_ha, X2014_Cropland_area_ha),
 									Area_Ranget0_to_Cropt1= c(X2009_RangetoCropland_area_ha, X2010_RangetoCropland_area_ha, X2011_RangetoCropland_area_ha, X2012_RangetoCropland_area_ha, X2013_RangetoCropland_area_ha, X2014_RangetoCropland_area_ha, X2015_RangetoCropland_area_ha),
@@ -123,6 +123,7 @@ CropRangeLCCLong <- rbind(with(CropRangeDF,
 									ADMIN_FIPS=rep(ADMIN_FIPS, 7), 
 									State=rep(STATE, 7), 
 									Year=rep(c(2009, 2010, 2011, 2012, 2013, 2014, 2015), each=nrow(CropRangeDF)),	
+									StableArea_noncrop= rep(Stable_noncrop, 7),
 									TotalRangeorCropAreainLCC_ha = rep(Total_Area_RangeorCrop_inLCC1to4, 7),
 									LCC = rep("LCC1to4", 7*nrow(LCC1to4Merge)),
 									Area_cropland = c(X2008_Cropland_area_ha, X2009_Cropland_area_ha, X2010_Cropland_area_ha, X2011_Cropland_area_ha, X2012_Cropland_area_ha, X2013_Cropland_area_ha, X2014_Cropland_area_ha),
@@ -137,10 +138,12 @@ CropRangeLCCLong <- rbind(with(CropRangeDF,
 #Calculate and transform RESPONSE VARIABLES
 ##########################
 setwd("Y:/Data/NCEAS_Postdoc/P4 ACR revised methods/")
-CropRangeLCCLong$PercentArea_Crop <- with(CropRangeLCCLong, Area_cropland/TotalRangeorCropAreainLCC_ha)	
+CropRangeLCCLong$PercentArea_CropStatic <- with(CropRangeLCCLong, 1-(StableArea_noncrop/TotalRangeorCropAreainLCC_ha)) #static. area of cropland is 1- area of rangeland that stays rangeland 2008 to 2012
+CropRangeLCCLong$PercentArea_Crop <- with(CropRangeLCCLong, Area_cropland/TotalRangeorCropAreainLCC_ha)	#time-varying. Ignores area that is intermittent cropland or uncertain classification
 CropRangeLCCLong[CropRangeLCCLong$Area_cropland==0, "PercentArea_Crop"] <- 0 #0 where there is no cropland in that county
 CropRangeLCCLong[is.na(CropRangeLCCLong$Area_cropland), "PercentArea_Crop"] <- 0 #0 where there is cropland is NA in that county
 CropRangeLCCLong[CropRangeLCCLong$TotalRangeorCropAreainLCC_ha==0, "PercentArea_Crop"] <- NA #NA where there is no land in that LCC category in that county
+CropRangeLCCLong[CropRangeLCCLong$TotalRangeorCropAreainLCC_ha==0, "PercentArea_CropStatic"] <- NA #NA where there is no land in that LCC category in that county
 
 CropRangeLCCLong$ConversionPropCropRangeforACR <- with(CropRangeLCCLong, Area_Ranget0_to_Cropt1/Area_Ranget0)
 write.csv(CropRangeLCCLong, paste0(getwd(), "/Analysis/tables/all data combined/LarkConversion_byLCC_GrasslandPrivateArea_forACR_allCounties.csv"), row.names=FALSE)
@@ -162,29 +165,38 @@ controlVariablesDF$PercRange_left_CRP[is.infinite(controlVariablesDF$PercRange_l
 #2. Add annual population change
 library(tidyr)
 popnDFto2009 <- read.csv("Data/Population_USCensus/co-est2009-alldata.csv", header=TRUE, stringsAsFactors=FALSE)
-popnDFto2009$ADMIN_FIPS <- paste0(popnDFto2009$STATE, sapply(popnDFto2009$COUNTY, function(x) formatC(x, width = 3, format = "d", flag = "0")))
-popnDFto2016 <- read.csv("Data/Population_USCensus/co-est2016-alldata.csv", header=TRUE, stringsAsFactors=FALSE)
-popnDFto2016$ADMIN_FIPS <- paste0(popnDFto2016$STATE, sapply(popnDFto2016$COUNTY, function(x) formatC(x, width = 3, format = "d", flag = "0")))
-popnDF <- merge(popnDFto2009, popnDFto2016, by="ADMIN_FIPS", all=TRUE) 
-popnDF1 <- popnDF[,c("ADMIN_FIPS", paste0("POPESTIMATE", 2008:2014))]
-popnDF2 <- popnDF[, c("ADMIN_FIPS", paste0("NPOPCHG_", 2008:2014))]
-names(popnDF1) <- c("ADMIN_FIPS", 2008:2014)
-names(popnDF2) <- c("ADMIN_FIPS", 2008:2014)
-popn1 <- gather(popnDF1, "Year", "Popn", 2:8)
-popn2 <- gather(popnDF2, "Year", "Popn_Chg", 2:8)
-popnDF <- merge(popn1, popn2, by=c("ADMIN_FIPS", "Year"), all=TRUE)	
-popnDF$PopnChg_Perc <- 100*popnDF$Popn_Chg/popnDF$Popn
-popnDF$nextYear = as.numeric(popnDF$Year)+1
-names(popnDF)[2] <- "censusYear"
+	popnDFto2009$ADMIN_FIPS <- paste0(popnDFto2009$STATE, sapply(popnDFto2009$COUNTY, function(x) formatC(x, width = 3, format = "d", flag = "0")))
+	popnDFto2016 <- read.csv("Data/Population_USCensus/co-est2016-alldata.csv", header=TRUE, stringsAsFactors=FALSE)
+	popnDFto2016$ADMIN_FIPS <- paste0(popnDFto2016$STATE, sapply(popnDFto2016$COUNTY, function(x) formatC(x, width = 3, format = "d", flag = "0")))
+	popnDF <- merge(popnDFto2009, popnDFto2016, by="ADMIN_FIPS", all=TRUE) 
+	popnDF1 <- popnDF[,c("ADMIN_FIPS", paste0("POPESTIMATE", 2008:2014))]
+	popnDF2 <- popnDF[, c("ADMIN_FIPS", paste0("NPOPCHG_", 2008:2014))]
+	names(popnDF1) <- c("ADMIN_FIPS", 2008:2014)
+	names(popnDF2) <- c("ADMIN_FIPS", 2008:2014)
+	popn1 <- gather(popnDF1, "Year", "Popn", 2:8)
+	popn2 <- gather(popnDF2, "Year", "Popn_Chg", 2:8)
+	popnDF <- merge(popn1, popn2, by=c("ADMIN_FIPS", "Year"), all=TRUE)	
+	popnDF$PopnChg_Perc <- 100*popnDF$Popn_Chg/popnDF$Popn
+	popnDF$nextYear = as.numeric(popnDF$Year)+1
+	names(popnDF)[2] <- "censusYear"
 write.csv(popnDF, "Data/Population_USCensus/Population_by_county_2008to2015.csv")	
 controlVariablesDF <- merge(controlVariablesDF, popnDF, by.x=c("ADMIN_FIPS","Year"), by.y=c("ADMIN_FIPS","nextYear"), all.x=TRUE)
 
 ###Static
-#3. Add area of irrigated (harvested ie cropland, not pastureland) land in each county
+#3a. Add area of irrigated (harvested ie cropland, not pastureland) land in each county
 # This is only available for whole county, not by lcc
-#Note the area irrigated is also available in the agDat, though this is the percent irrigated for all agricultural land (cropland, rangeland & pasture)
 irrigatedAreaDF <- read.csv("Data/USDA_IrrigatedArea/CroplandArea_Irrigation_USDACensus_byCounty_formatted.csv", header=TRUE)
 controlVariablesDF <- merge(controlVariablesDF, irrigatedAreaDF, by="ADMIN_FIPS",all.x=TRUE)
+#3b. Area irrigated is also available in the agDat, though this is the percent irrigated for all agricultural land (cropland, rangeland & pasture)
+agDat <- read.csv("Data/USDA AgCensus/AllCountyData_AgCensus2012.csv", header=TRUE, stringsAsFactors=FALSE)
+	agNameLookup <- read.csv("Data/USDA AgCensus/Ag_Census_Map_data_variablelookup_07172015.csv", header=TRUE, stringsAsFactors=FALSE) 
+	#rename columns
+	for (i in 2:ncol(agDat)){
+		names(agDat)[i] <- agNameLookup[which(agNameLookup$MapID == strsplit(names(agDat)[i], "_valueNumeric")[[1]][1]), "MAPTITLE"]
+		} 
+		names(agDat)[which(names(agDat)=="Acres of Irrigated Land as Percent of Land in Farms Acreage:  2012")] <- "PercentLandIrrigated"
+controlVariablesDF <- merge(controlVariablesDF, agDat[, c("FipsNumeric","PercentLandIrrigated")], by.x="ADMIN_FIPS", by.y="FipsNumeric", all.x=TRUE)
+
 
 ###Static
 #4. Add Area & % urban (urbanised plus urban clusters)
@@ -226,72 +238,76 @@ if(x %in% c(2008:2011)){
 	
 
 #aggregate(controlVariablesDF[,c("TotalRangeorCropAreainLCC_ha", "Area_cropland", "Area_Ranget0_to_Cropt1", "Area_Ranget0_to_Ranget1", "Area_Ranget0", "PercentArea_Crop", "ConversionPropCropRangeforACR", "Acres_left_CRP", "Popn", "Popn_Chg", "PopnChg_Perc", "PercentCroplandthatisIrrigated", "AREA_URBAN_ha", "AREAPCT_URBAN")], by = controlVariablesDF[,c("ADMIN_FIPS", "LCC", "TwoyrAverage")], FUN=mean, na.rm=TRUE)
+
+#Time-varying proportional values are expressed as annual change i.e. Conversion rate is expressed as the annual rate, averaged across the time period, as is PopnChg_Perc & PercentArea_Crop. Area values that are measured across the time period (numerators) are summed across the whole time period i.e. Area_ranget0_to_Cropt1, Popn_Chng is summed across 2008 to 2012 for instance. Area values that are measured at the beginning of the time period (denominators) are time-averaged i.e. Area_ranget0, Popn.
 	
 #TWO Year lags		
 twoYearMeans <-  ddply(controlVariablesDF, .(ADMIN_FIPS, LCC, TwoyrAverage), summarize,
+		ConversionPropCropRangeforACR = mean(ConversionPropCropRangeforACR, na.rm=TRUE),
 		TotalRangeorCropAreainLCC_ha = round(mean(TotalRangeorCropAreainLCC_ha, na.rm=TRUE), 2),
 		Area_Ranget0_to_Cropt1 = sum(Area_Ranget0_to_Cropt1, na.rm=TRUE),
 		Area_cropland = round(mean(Area_cropland, na.rm=TRUE), 2),
 		Area_Ranget0_to_Ranget1 = round(mean(Area_Ranget0_to_Ranget1, na.rm=TRUE), 2),
 		Area_Ranget0 = round(mean(Area_Ranget0, na.rm=TRUE), 2),
+		PercentArea_CropStatic = round(mean(PercentArea_CropStatic, na.rm=TRUE), 4),
 		PercentArea_Crop = round(mean(PercentArea_Crop, na.rm=TRUE), 4),
 		Acres_left_CRP = sum(Acres_left_CRP, na.rm=TRUE),
+		PercRange_left_CRP = mean(PercRange_left_CRP, na.rm=TRUE),
 		Popn = round(mean(Popn, na.rm=TRUE), 0),
 		Popn_Chg = sum(Popn_Chg, na.rm=TRUE),
+		PopnChg_Perc = mean(PopnChg_Perc, na.rm=TRUE),
+		PercentLandIrrigated = mean(PercentLandIrrigated, na.rm=TRUE),
 		PercentCroplandthatisIrrigated = mean(PercentCroplandthatisIrrigated, na.rm=TRUE),
 		Cropland_Harvested_Irrigated_Acres = mean(Cropland_Harvested_Irrigated_Acres, na.rm=TRUE),
 		AREA_URBAN_ha = round(mean(AREA_URBAN_ha, na.rm=TRUE), 2),
 		AREAPCT_URBAN = round(mean(AREAPCT_URBAN, na.rm=TRUE), 4)
 		)
-twoYearMeans$ConversionPropCropRangeforACR <- with(twoYearMeans, Area_Ranget0_to_Cropt1/Area_Ranget0)
-twoYearMeans$Popn_Chg_Perc <- with(twoYearMeans, Popn_Chg/Popn)
-twoYearMeans$PercRange_left_CRP <- with(twoYearMeans, Acres_left_CRP*0.404686/Area_Ranget0)
-twoYearMeans$PercRange_left_CRP[is.infinite(twoYearMeans$PercRange_left_CRP)] <- 0
 write.csv(twoYearMeans, paste0(getwd(), "/Analysis/tables/all data combined/LandConversion_combinedData_allUSStates_byLCC_plusControlVariables_2yraverages.csv"), row.names=FALSE)
 
 
 #THREE Year lags		
 threeYearMeans <-  ddply(controlVariablesDF, .(ADMIN_FIPS, LCC, ThreeyrAverage), summarize,
+		ConversionPropCropRangeforACR = mean(ConversionPropCropRangeforACR, na.rm=TRUE),
 		TotalRangeorCropAreainLCC_ha = round(mean(TotalRangeorCropAreainLCC_ha, na.rm=TRUE), 2),
 		Area_Ranget0_to_Cropt1 = sum(Area_Ranget0_to_Cropt1, na.rm=TRUE),
 		Area_cropland = round(mean(Area_cropland, na.rm=TRUE), 2),
 		Area_Ranget0_to_Ranget1 = round(mean(Area_Ranget0_to_Ranget1, na.rm=TRUE), 2),
 		Area_Ranget0 = round(mean(Area_Ranget0, na.rm=TRUE), 2),
+		PercentArea_CropStatic = round(mean(PercentArea_CropStatic, na.rm=TRUE), 4),
 		PercentArea_Crop = round(mean(PercentArea_Crop, na.rm=TRUE), 4),
+		PercRange_left_CRP = mean(PercRange_left_CRP, na.rm=TRUE),
 		Acres_left_CRP = sum(Acres_left_CRP, na.rm=TRUE),
 		Popn = round(mean(Popn, na.rm=TRUE), 0),
 		Popn_Chg = sum(Popn_Chg, na.rm=TRUE),
+		PopnChg_Perc = mean(PopnChg_Perc, na.rm=TRUE),
+		PercentLandIrrigated = mean(PercentLandIrrigated, na.rm=TRUE),
 		PercentCroplandthatisIrrigated = mean(PercentCroplandthatisIrrigated, na.rm=TRUE),
 		Cropland_Harvested_Irrigated_Acres = mean(Cropland_Harvested_Irrigated_Acres, na.rm=TRUE),
 		AREA_URBAN_ha = round(mean(AREA_URBAN_ha, na.rm=TRUE), 2),
 		AREAPCT_URBAN = round(mean(AREAPCT_URBAN, na.rm=TRUE), 4)
 		)
-threeYearMeans$ConversionPropCropRangeforACR <- with(threeYearMeans, Area_Ranget0_to_Cropt1/Area_Ranget0)
-threeYearMeans$Popn_Chg_Perc <- with(threeYearMeans, Popn_Chg/Popn)
-threeYearMeans$PercRange_left_CRP <- with(threeYearMeans, Acres_left_CRP*0.404686/Area_Ranget0)
-threeYearMeans$PercRange_left_CRP[is.infinite(threeYearMeans$PercRange_left_CRP)] <- 0
 write.csv(threeYearMeans, paste0(getwd(), "/Analysis/tables/all data combined/LandConversion_combinedData_allUSStates_byLCC_plusControlVariables_3yraverages.csv"), row.names=FALSE)
 
 #FOUR Year lags		
 fourYearMeans <-  ddply(controlVariablesDF, .(ADMIN_FIPS, LCC, FouryrAverage), summarize,
+		ConversionPropCropRangeforACR = mean(ConversionPropCropRangeforACR, na.rm=TRUE),
 		TotalRangeorCropAreainLCC_ha = round(mean(TotalRangeorCropAreainLCC_ha, na.rm=TRUE), 2),
 		Area_Ranget0_to_Cropt1 = sum(Area_Ranget0_to_Cropt1, na.rm=TRUE),
 		Area_cropland = round(mean(Area_cropland, na.rm=TRUE), 2),
 		Area_Ranget0_to_Ranget1 = round(mean(Area_Ranget0_to_Ranget1, na.rm=TRUE), 2),
 		Area_Ranget0 = round(mean(Area_Ranget0, na.rm=TRUE), 2),
+		PercentArea_CropStatic = round(mean(PercentArea_CropStatic, na.rm=TRUE), 4),
 		PercentArea_Crop = round(mean(PercentArea_Crop, na.rm=TRUE), 4),
+		PercRange_left_CRP = mean(PercRange_left_CRP, na.rm=TRUE),
 		Acres_left_CRP = sum(Acres_left_CRP, na.rm=TRUE),
 		Popn = round(mean(Popn, na.rm=TRUE), 0),
 		Popn_Chg = sum(Popn_Chg, na.rm=TRUE),
-		PercentCroplandthatisIrrigated = mean(PercentCroplandthatisIrrigated, na.rm=TRUE),
+		PopnChg_Perc = mean(PopnChg_Perc, na.rm=TRUE),
+		PercentLandIrrigated = mean(PercentLandIrrigated, na.rm=TRUE),PercentCroplandthatisIrrigated = mean(PercentCroplandthatisIrrigated, na.rm=TRUE),
 		Cropland_Harvested_Irrigated_Acres = mean(Cropland_Harvested_Irrigated_Acres, na.rm=TRUE),
 		AREA_URBAN_ha = round(mean(AREA_URBAN_ha, na.rm=TRUE), 2),
 		AREAPCT_URBAN = round(mean(AREAPCT_URBAN, na.rm=TRUE), 4)
 		)
-fourYearMeans$ConversionPropCropRangeforACR <- with(fourYearMeans, Area_Ranget0_to_Cropt1/Area_Ranget0)
-fourYearMeans$Popn_Chg_Perc <- with(fourYearMeans, Popn_Chg/Popn)
-fourYearMeans$PercRange_left_CRP <- with(fourYearMeans, Acres_left_CRP*0.404686/Area_Ranget0)
-fourYearMeans$PercRange_left_CRP[is.infinite(fourYearMeans$PercRange_left_CRP)] <- 0
 write.csv(fourYearMeans, paste0(getwd(), "/Analysis/tables/all data combined/LandConversion_combinedData_allUSStates_byLCC_plusControlVariables_4yraverages.csv"), row.names=FALSE)
 
 ###########################
